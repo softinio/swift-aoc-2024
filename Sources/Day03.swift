7fee279ab6fa19fccdeee46e33d5213e129a5d41
Salar Rahmanian <code@softinio.com>
2024-12-15 11:33:31 -0800

    Completed Day 3 Part 2

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -25,6 +25,31 @@ struct Day03: AdventDay {
     return extractedTuples
   }
 
+  var filteredEntities: [(Int, Int)] {
+    let pattern = "(don't\\(\\))|(do\\(\\))|mul\\((\\d+),(\\d+)\\)"
+    var extractedTuples: [(Int, Int)] = []
+    var inputFunc = true
+    if let regex = try? NSRegularExpression(pattern: pattern) {
+      let matches = regex.matches(
+        in: data, options: [], range: NSRange(location: 0, length: data.count))
+      for match in matches {
+        if let dontRange = Range(match.range(at: 1), in: data), data[dontRange] == "don't()" {
+          inputFunc = false
+        } else if let doRange = Range(match.range(at: 2), in: data), data[doRange] == "do()" {
+          inputFunc = true
+        } else if inputFunc,
+          let range1 = Range(match.range(at: 3), in: data),
+          let range2 = Range(match.range(at: 4), in: data),
+          let first = Int(data[range1]),
+          let second = Int(data[range2])
+        {
+          extractedTuples.append((first, second))
+        }
+      }
+    }
+    return extractedTuples
+  }
+
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
     // Calculate the sum of the first set of input data
@@ -36,7 +61,8 @@ struct Day03: AdventDay {
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
     // Sum the maximum entries in each set of data
-    //entities.map { $0.max() ?? 0 }.reduce(0, +)
-    return true
+    filteredEntities.reduce(0) { (sum, tuple) in
+      sum + tuple.0 * tuple.1
+    }
   }
 }
